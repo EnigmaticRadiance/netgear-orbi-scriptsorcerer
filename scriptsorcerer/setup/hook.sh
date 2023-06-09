8838264a041145c7a4442928f022f44091b6e3f2
EnigmaticRadiance <135998080+EnigmaticRadiance@users.noreply.github.com>
2023-06-09 16:08:56 -0700

    fix infinite loop & hook logic error

--- scriptsorcerer/setup/hook.sh
+++ scriptsorcerer/setup/hook.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
-#I just feel like something here is flawed
 
 if [ -e "/mnt/bitdefender/bin/bd_procd" ]; then
     if [ ! -e "/mnt/bitdefender/bin/bd_procd.bak" ]; then
-        cp "/mnt/bitdefender/bin/bd_procd" "/mnt/bitdefender/bin/bd_procd.bak" && echo "Backup created: /mnt/bitdefender/bin/bd_procd.bak"
-    else
+        #should be initial install
+        cp "/mnt/bitdefender/bin/bd_procd" "/mnt/bitdefender/bin/bd_procd.bak" && echo "Backup created: /mnt/bitdefender/bin/bd_procd.bak"         
+        cp /mnt/ntgr/scriptsorcerer/setup/bd_procd.sh /mnt/bitdefender/bin/bd_procd 
+    elif grep -q "scriptsorcerer" "/mnt/bitdefender/bin/bd_procd"; then
         echo "already hooked. lets update it."
-        mv /mnt/ntgr/scriptsorcerer/setup/bd_procd.sh /mnt/bitdefender/bin/bd_procd
+        cp /mnt/ntgr/scriptsorcerer/setup/bd_procd.sh /mnt/bitdefender/bin/bd_procd
     fi
-    mv /mnt/ntgr/scriptsorcerer/setup/bd_procd.sh /mnt/bitdefender/bin/bd_procd 
 else
     echo "Source file not found: /mnt/bitdefender/bin/bd_procd"
 fi
