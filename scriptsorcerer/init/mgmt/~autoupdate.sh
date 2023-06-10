32dd6944860423a6891670c409ad6eec96b9c6ce
EnigmaticRadiance <135998080+EnigmaticRadiance@users.noreply.github.com>
2023-06-09 17:06:38 -0700

    autoupdate fixes

--- scriptsorcerer/init/mgmt/~autoupdate.sh
+++ scriptsorcerer/init/mgmt/~autoupdate.sh
@@ -5,7 +5,7 @@
 #I would personally recommend NOT enabling it. auto-updating from the github repo almost certainly makes your router less secure.
 #Even though I would never intentionally put anything malicious in it.
 #Just manually run it if you see a new update and care to update it. 
-#TODO auto-update scripts? maybe include a shebang type of thing with a url & version number?
+#TODO auto-update scripts? maybe include a shebang type of thing with a url & version number? HOOK INTO CRONTAB OR SMTH SO IT DOESN'T ONLY UPDATE ON BOOT!
 
 directory="/mnt/ntgr/scriptsorcerer"
 current_release_tag=$(cat $directory/ver)
@@ -15,12 +15,12 @@ if [[ "$current_release_tag" != "$latest_release_tag" ]]; then
     latest_release_url="https://github.com/EnigmaticRadiance/netgear-orbi-scriptsorcerer/archive/refs/tags/$latest_release_tag.zip"
     curl -sL "$latest_release_url" -o /tmp/scriptsorcererupd.zip
     unzip /tmp/scriptsorcererupd.zip -d /tmp/scriptsorcererupd
-    mv -f /tmp/scriptsorcererupd/netgear-orbi-scriptsorcerer-"$latest_release_tag"/scriptsorcerer "$directory"
+    cp -R -f "/tmp/scriptsorcererupd/netgear-orbi-scriptsorcerer-$latest_release_tag/scriptsorcerer/." "$directory/" 
     rm /tmp/scriptsorcererupd.zip
     rm -r /tmp/scriptsorcererupd
     echo $latest_release_tag > $directory/ver
     find "$directory" -type f -name "*.sh" -exec chmod a+x {} +
-    if [[ $(basename "$0") != "~"* ]]; then
+    if ! basename "$0" | grep -q '~'; then
         #this is so awful lmfao
         mv $directory/init/mgmt/~autoupdate.sh $directory/init/mgmt/autoupdate.sh
     fi
